dc1c017e557b4a9eb568af48d87f89ac908dc45e
Abbath <pitongogi@gmail.com>
2025-08-22 23:19:43 +0200

    Refactoring.

--- app/Main.hs
+++ app/Main.hs
@@ -9,39 +9,45 @@ import Data.Function (on)
 import Data.Map (Map, (!))
 import Data.Text (Text)
 import System.IO (IOMode (WriteMode), hPutStr, hPutStrLn, withFile)
-import System.Random (StdGen, newStdGen, randomR)
+import System.Random.Stateful
 
 data Expr = Param Text | Num Double | Add Expr Expr | Mul Expr Expr deriving (Show)
 
-generateFunction :: [Text] -> StdGen -> Expr
-generateFunction ps = fst . go ps
+generateFunction :: (StatefulGen g m) => [Text] -> g -> m Expr
+generateFunction ps gen = do
+  choice <- uniformRM (1 :: Int, 4) gen
+  case choice of
+    1 -> do
+      arg <- uniformRM (0 :: Int, 1) gen
+      pure . Num . fromIntegral $ arg
+    2 -> do
+      idx <- uniformRM (0 :: Int, length ps - 1) gen
+      pure . Param . (!! idx) $ ps
+    3 -> do
+      r1 <- gf gen
+      r2 <- gf gen
+      pure $ Add r1 r2
+    4 -> do
+      r1 <- gf gen
+      r2 <- gf gen
+      pure $ Mul r1 r2
+    _ -> error "Unreachable!"
  where
-  go p g =
-    let (r, g1) = randomR (1 :: Int, 4) g
-     in case r of
-          1 -> let (arg, g2) = randomR (0 :: Int, 1) g1 in (Num $ fromIntegral arg, g2)
-          2 -> let (idx, g2) = randomR (0 :: Int, length p - 1) g1 in (Param $ p !! idx, g2)
-          3 ->
-            let (r1, g2) = go p g1
-                (r2, g3) = go p g2
-             in (Add r1 r2, g3)
-          4 ->
-            let (r1, g2) = go p g1
-                (r2, g3) = go p g2
-             in (Mul r1 r2, g3)
-          _ -> error "Unreachable!"
+  gf = generateFunction ps
 
 computeFunction :: Map Text Double -> Expr -> Double
 computeFunction m e = case e of
   Num x -> x
   Param n -> m ! n
-  Add e1 e2 -> computeFunction m e1 + computeFunction m e2
-  Mul e1 e2 -> computeFunction m e1 * computeFunction m e2
+  Add e1 e2 -> cf e1 + cf e2
+  Mul e1 e2 -> cf e1 * cf e2
+ where
+  cf = computeFunction m
 
 main :: IO ()
 main = do
-  gen <- newStdGen
-  let fun = generateFunction ["x", "y"] gen
+  gen <- newStdGen >>= newIOGenM
+  fun <- generateFunction ["x", "y"] gen
   print fun
   let size = 1000 :: Int
   let values =
@@ -54,7 +60,7 @@ main = do
           ([0 .. size * size - 1] :: [Int])
   let maxValue = fst $ maximumBy (compare `on` fst) values
   print maxValue
-  withFile "dupa.pgm" WriteMode $ \h -> do
+  withFile "image.pgm" WriteMode $ \h -> do
     hPutStrLn h "P2"
     hPutStrLn h $ show size <> " " <> show size
     hPutStrLn h "255"
