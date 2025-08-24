efe7b593bb9fe2f4d8ac3675766c4cca33c5eef3
Abbath <pitongogi@gmail.com>
2025-08-24 15:26:33 +0200

    Weights.

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE OverloadedStrings #-}
 
@@ -9,6 +10,7 @@ import Data.Foldable (maximumBy, minimumBy)
 import Data.Function (on)
 import Data.Map (Map, (!))
 import Data.Text (Text, pack)
+import Data.Text qualified as T
 import Data.Text.IO qualified as T
 import Options.Applicative
 import System.IO (IOMode (WriteMode), hPutStrLn, hSetBinaryMode, withFile)
@@ -27,25 +29,24 @@ prettyPrint (Mul e1 e2) = "(" <> prettyPrint e1 <> " * " <> prettyPrint e2 <> ")
 prettyPrint (Pow e1) = "(" <> prettyPrint e1 <> "^2)"
 prettyPrint (Fun f e1) = f <> "(" <> prettyPrint e1 <> ")"
 
-generateFunction :: (StatefulGen g m) => Int -> Double -> [Text] -> g -> m Expr
-generateFunction depth mc ps gen
+generateFunction :: (StatefulGen g m) => Int -> Double -> [Text] -> Weights -> g -> m Expr
+generateFunction depth mc ps ws gen
   | depth == 0 = do
       choice <- uniformRM (0 :: Int, 1) gen
       if choice == 0
         then Num <$> randomNumber
         else Param <$> randomChoice ps
   | otherwise = do
-      choice <- uniformRM (1 :: Int, 6) gen
-      case choice of
-        1 -> Num <$> randomNumber
-        2 -> Param <$> randomChoice ps
-        3 -> Add <$> gf <*> gf
-        4 -> Mul <$> gf <*> gf
-        5 -> Pow <$> gf
-        6 -> Fun <$> randomChoice ["sin", "abs", "sqrt", "log"] <*> gf
-        _ -> error "Unreachable!"
+      choice <- uniformRM (0.0 :: Double, 1.0) gen
+      if
+        | choice < w1 ws -> Num <$> randomNumber
+        | choice < w2 ws -> Param <$> randomChoice ps
+        | choice < w3 ws -> Add <$> gf <*> gf
+        | choice < w4 ws -> Mul <$> gf <*> gf
+        | choice < w5 ws -> Pow <$> gf
+        | otherwise -> Fun <$> randomChoice ["sin", "abs", "sqrt", "log"] <*> gf
  where
-  gf = generateFunction (depth - 1) mc ps gen
+  gf = generateFunction (depth - 1) mc ps ws gen
   randomNumber = uniformRM (0 :: Double, mc) gen
   randomChoice v = do
     idx <- uniformRM (0 :: Int, length v - 1) gen
@@ -77,6 +78,7 @@ data Options = Options
   , imageWidth :: Int
   , imageHeight :: Int
   , maxConstant :: Double
+  , weights :: Text
   , output :: Output
   }
 
@@ -88,7 +90,20 @@ options =
     <*> option auto (long "width" <> short 'w' <> help "Width in pixels" <> showDefault <> value 1024 <> metavar "WIDTH")
     <*> option auto (long "height" <> short 'h' <> help "Height in pixels" <> showDefault <> value 1024 <> metavar "HEIGHT")
     <*> option auto (long "max-constant" <> short 'c' <> help "Maximum constant range" <> showDefault <> value 10.0 <> metavar "MAX_CONSTANT")
+    <*> strOption (long "weights" <> short 'g' <> help "Weights" <> showDefault <> value "1 1 1 1 1 1" <> metavar "WEIGHTS")
     <*> flag TextOutput BinaryOutput (long "binary-output" <> short 'b')
+
+defaultWeights :: Weights
+defaultWeights = Weights 1 1 1 1 1 1
+
+data Weights = Weights
+  { w1 :: Double
+  , w2 :: Double
+  , w3 :: Double
+  , w4 :: Double
+  , w5 :: Double
+  , w6 :: Double
+  }
 
 main :: IO ()
 main = do
@@ -96,11 +111,13 @@ main = do
   gen_r <- newStdGen >>= newIOGenM
   gen_g <- newStdGen >>= newIOGenM
   gen_b <- newStdGen >>= newIOGenM
-  fun_r <- generateFunction (maxDepth args) (maxConstant args) ["x", "y"] gen_r
+  let ws = read @[Double] . T.unpack . ("[" <>) . (<> "]") . T.intercalate ", " . T.words $ weights args
+  let normWeights = normalizeWieghts (Weights (head ws) (ws !! 1) (ws !! 2) (ws !! 3) (ws !! 4) (ws !! 5))
+  fun_r <- generateFunction (maxDepth args) (maxConstant args) ["x", "y"] normWeights gen_r
   T.putStrLn $ prettyPrint fun_r
-  fun_g <- generateFunction (maxDepth args) (maxConstant args) ["x", "y"] gen_g
+  fun_g <- generateFunction (maxDepth args) (maxConstant args) ["x", "y"] normWeights gen_g
   T.putStrLn $ prettyPrint fun_g
-  fun_b <- generateFunction (maxDepth args) (maxConstant args) ["x", "y"] gen_b
+  fun_b <- generateFunction (maxDepth args) (maxConstant args) ["x", "y"] normWeights gen_b
   T.putStrLn $ prettyPrint fun_b
   let width = imageWidth args
   let height = imageHeight args
@@ -143,4 +160,14 @@ main = do
   compute v minValue valueSpan = (`mod` 256) . truncate $ (v - minValue) / valueSpan * 255
   computeBound f g values = f $ g (compare `on` f) values
   computeBounds f values = (computeBound f maximumBy values, computeBound f minimumBy values)
+  normalizeWieghts ws =
+    let
+      s1 = w1 ws
+      s2 = w1 ws + w2 ws
+      s3 = w1 ws + w2 ws + w3 ws
+      s4 = w1 ws + w2 ws + w3 ws + w4 ws
+      s5 = w1 ws + w2 ws + w3 ws + w4 ws + w5 ws
+      s6 = w1 ws + w2 ws + w3 ws + w4 ws + w5 ws + w6 ws
+     in
+      Weights (s1 / s6) (s2 / s6) (s3 / s6) (s4 / s6) (s5 / s6) (s6 / s6)
   opts = info (options <**> helper) (fullDesc <> progDesc "Function Visualisation" <> header "Visualise a random function")
