cc807331c1459ab8de92668bd2efbf2bdb3d12c4
Abbath <pitongogi@gmail.com>
2025-09-05 15:43:50 +0200

    Optimization.

--- app/Main.hs
+++ app/Main.hs
@@ -11,7 +11,6 @@ import Data.Text qualified as T
 import Data.Text.IO qualified as T
 import Data.Vector qualified as V
 import Data.Vector.Storable qualified as VS
-import Data.Word
 import ExprParser (Cond (..), Expr (..), FunType (..), parseExpr)
 import Options.Applicative
 import System.FilePath (replaceBaseName, takeBaseName, (-<.>))
@@ -182,35 +181,34 @@ perform args idx = do
       then gf gen_b
       else pure . parseFunction $ funB args
   unless (singleFunction args) $ T.putStrLn $ prettyPrint fun_b
-  let width = imageWidth args
-  let height = imageHeight args
-  let d c s = fromIntegral c / fromIntegral s * fieldSize args - fieldSize args / 2
-  let funs = [fun_r, fun_g, fun_b] :: [Expr]
-  let !values =
+  let !width = imageWidth args
+  let !height = imageHeight args
+  let !fs = fieldSize args
+  let d c s = fromIntegral c / fromIntegral s * fs - fs / 2
+  let funs = if singleFunction args then [fun_r, fun_r, fun_r] else [fun_r, fun_g, fun_b] :: V.Vector Expr
+  let values =
         V.generate
           (width * height * 3)
           ( \n ->
-              let i = n `mod` 3
-                  (x, y) = (n `div` 3) `divMod` width
-                  (xd, yd) = (d x width, d y height)
-                  cfp = computeFunction (xd, yd)
-               in if singleFunction args then cfp (head funs) else cfp (funs !! i)
+              let (m, i) = n `divMod` 3
+                  (x, y) = m `divMod` width
+               in computeFunction (d x width, d y height) (funs V.! i)
           )
   let (maxValue_r, minValue_r) = computeBounds (V.ifilter (\i _ -> i `mod` 3 == 0) values)
-  let valueSpan_r = maxValue_r - minValue_r
+  let valueSpan_r = (maxValue_r - minValue_r) / 255
   let (maxValue_g, minValue_g) = computeBounds (V.ifilter (\i _ -> i `mod` 3 == 1) values)
-  let valueSpan_g = maxValue_g - minValue_g
+  let valueSpan_g = (maxValue_g - minValue_g) / 255
   let (maxValue_b, minValue_b) = computeBounds (V.ifilter (\i _ -> i `mod` 3 == 2) values)
-  let valueSpan_b = maxValue_b - minValue_b
+  let valueSpan_b = (maxValue_b - minValue_b) / 255
   let filename =
         if idx == -1
           then output args
           else replaceBaseName (output args) (takeBaseName (output args) <> "_" <> printf "%03d" idx)
   do
-    let !values_storable =
+    let values_storable =
           VS.convert $
             V.imap
-              ( \i v -> case i `mod` 3 of
+              ( \i v -> truncate $ case i `mod` 3 of
                   0 -> compute v minValue_r valueSpan_r
                   1 -> compute v minValue_g valueSpan_g
                   2 -> compute v minValue_b valueSpan_b
@@ -219,9 +217,9 @@ perform args idx = do
               values
     J.writePng @J.PixelRGB8 (filename -<.> "png") (J.Image width height values_storable)
  where
-  compute :: Double -> Double -> Double -> Word8
-  compute v minValue valueSpan = truncate $ (v - minValue) / valueSpan * 255
-  computeBounds values = (maximum values, minimum values)
+  compute :: Double -> Double -> Double -> Double
+  compute v minValue valueSpan = (v - minValue) / valueSpan
+  computeBounds values = (V.maximum values, V.minimum values)
   normalizeWieghts ws =
     let
       s7 = w1 ws + w2 ws + w3 ws + w4 ws + w5 ws + w6 ws + w7 ws
