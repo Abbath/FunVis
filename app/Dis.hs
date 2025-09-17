6b983de4692a10811d35f390a0d4df2e8ffce477
Abbath <pitongogi@gmail.com>
2025-09-17 15:03:27 +0200

    Discord options.

--- app/Dis.hs
+++ app/Dis.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Dis where
+module Dis (discordImage) where
 
 import Configuration.Dotenv (defaultConfig, loadFile)
 import Control.Monad (void, when)
@@ -8,8 +8,7 @@ import Control.Monad.IO.Class (MonadIO (liftIO))
 import Data.ByteString qualified as BS
 import Data.Char (isSpace)
 import Data.Maybe (fromMaybe)
-import Data.Text as TS (dropWhile, isPrefixOf, pack, toLower)
-import Data.Text.IO as TSIO (putStrLn)
+import Data.Text as T
 import Data.Text.IO qualified as TIO
 import Discord (
   DiscordHandler,
@@ -20,21 +19,22 @@ import Discord (
  )
 import Discord.Requests qualified as R
 import Discord.Types (Event (..), Message (messageTimestamp), User (userName), messageAuthor, messageChannelId, messageContent, messageId, userIsBot)
+import Options.Applicative
 import Performer (generateFunctions, perform)
 import System.Environment (lookupEnv)
 import Types
 
 -- | Replies "pong" to every message that starts with "ping"
-discordImage :: Options -> IO ()
-discordImage opts = do
+discordImage :: IO ()
+discordImage = do
   TIO.putStrLn "Started server"
   loadFile defaultConfig
   token <- fromMaybe "" <$> lookupEnv "DISCORD_TOKEN"
   userFacingError <-
     runDiscord $
       def
-        { discordToken = TS.pack token
-        , discordOnEvent = eventHandler opts
+        { discordToken = T.pack token
+        , discordOnEvent = eventHandler
         , discordOnLog = \s -> TIO.putStrLn s >> TIO.putStrLn ""
         } -- if you see OnLog error, post in the discord / open an issue
   TIO.putStrLn userFacingError
@@ -42,24 +42,31 @@ discordImage opts = do
 -- userFacingError is an unrecoverable error
 -- put normal 'cleanup' code in discordOnEnd (see examples)
 
-eventHandler :: Options -> Event -> DiscordHandler ()
-eventHandler opts event =
+eventHandler :: Event -> DiscordHandler ()
+eventHandler event =
   case event of
     MessageCreate m -> when (isCalc m && not (fromBot m)) $ do
-      liftIO . TSIO.putStrLn $ "[" <> (TS.pack . show $ messageTimestamp m) <> "] " <> userName (messageAuthor m) <> ": " <> TS.dropWhile (not . isSpace) (messageContent m)
-      void $ restCall (R.CreateReaction (messageChannelId m, messageId m) "eyes")
-      respond m "Ololo"
-      funs <- liftIO $ generateFunctions opts
-      liftIO $ perform funs opts (-1)
-      image_data <- liftIO $ BS.readFile "image.png"
-      let mdopts = def @R.MessageDetailedOpts
-      void . restCall $
-        R.CreateMessageDetailed (messageChannelId m) $
-          mdopts
-            { R.messageDetailedFile =
-                Just
-                  ("image.png", image_data)
-            }
+      let message_text = messageContent m
+      let parseResult = execParserPure defaultPrefs (info (options <**> helper) fullDesc) . parseCommands $ T.drop 6 message_text
+      let maybe_opts = getParseResult parseResult
+      liftIO $ print maybe_opts
+      case maybe_opts of
+        Just opts -> do
+          liftIO . TIO.putStrLn $ "[" <> (T.pack . show $ messageTimestamp m) <> "] " <> userName (messageAuthor m) <> ": " <> T.dropWhile (not . isSpace) (messageContent m)
+          void $ restCall (R.CreateReaction (messageChannelId m, messageId m) "eyes")
+          respond m "Ololo"
+          funs <- liftIO $ generateFunctions opts
+          liftIO $ perform funs opts (-1)
+          image_data <- liftIO $ BS.readFile "image.png"
+          let mdopts = def @R.MessageDetailedOpts
+          void . restCall $
+            R.CreateMessageDetailed (messageChannelId m) $
+              mdopts
+                { R.messageDetailedFile =
+                    Just
+                      ("image.png", image_data)
+                }
+        Nothing -> respond m "Wrong options"
     _ -> return ()
  where
   respond m rsp =
@@ -74,4 +81,13 @@ fromBot :: Message -> Bool
 fromBot = userIsBot . messageAuthor
 
 isCalc :: Message -> Bool
-isCalc = (\m -> "!image" `TS.isPrefixOf` m) . TS.toLower . messageContent
+isCalc = (\m -> "!image" `T.isPrefixOf` m) . T.toLower . messageContent
+
+parseCommands :: T.Text -> [String]
+parseCommands = go
+ where
+  go m | T.null m = []
+  go m = case T.head m of
+    ' ' -> let (x, xs) = T.breakOn " " (T.tail m) in T.unpack x : go xs
+    '"' -> let (x, xs) = T.breakOn "\"" (T.tail m) in T.unpack x : go xs
+    _ -> []
--- app/Types.hs
+++ app/Types.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Types where
 
 import Data.Text (Text)
+import Options.Applicative
 
 data Options = Options
   { maxDepth :: Int
@@ -20,3 +23,24 @@ data Options = Options
   , step :: Double
   , discord :: Bool
   }
+  deriving (Show)
+
+options :: Parser Options
+options =
+  Options
+    <$> option auto (long "max-depth" <> short 'm' <> help "Maximum function depth" <> showDefault <> value 7 <> metavar "DEPTH")
+    <*> option auto (long "field-size" <> short 's' <> help "Field size" <> showDefault <> value (2 * pi) <> metavar "FSIZE")
+    <*> option auto (long "width" <> short 'w' <> help "Width in pixels" <> showDefault <> value 1024 <> metavar "WIDTH")
+    <*> option auto (long "height" <> short 'h' <> help "Height in pixels" <> showDefault <> value 1024 <> metavar "HEIGHT")
+    <*> option auto (long "max-constant" <> short 'c' <> help "Maximum constant range" <> showDefault <> value 10.0 <> metavar "MAX_CONSTANT")
+    <*> strOption (long "weights" <> short 't' <> help "Weights" <> showDefault <> value "1 1 1 1 1 1 1" <> metavar "WEIGHTS")
+    <*> strOption (long "output" <> short 'o' <> help "Output file name" <> showDefault <> value "image.png" <> metavar "OUTPUT")
+    <*> strOption (long "red" <> short 'r' <> help "Red channel function" <> value "" <> metavar "RED_FUNCTION")
+    <*> strOption (long "green" <> short 'g' <> help "Green channel function" <> value "" <> metavar "GREEN_FUNCTION")
+    <*> strOption (long "blue" <> short 'b' <> help "Blue channel function" <> value "" <> metavar "BLUE_FUNCTION")
+    <*> strOption (long "alpha" <> short 'a' <> help "Alpha channel function" <> value "" <> metavar "ALPHA_FUNCTION")
+    <*> option auto (long "attempts" <> short 'n' <> help "Number of attempts" <> showDefault <> value 0 <> metavar "ATTEMPTS")
+    <*> switch (long "single-function" <> short 'l' <> help "Single function")
+    <*> switch (long "use-alpha" <> short 'p' <> help "Use alpha")
+    <*> option auto (long "step" <> short 'e' <> help "Time step" <> showDefault <> value 0 <> metavar "STEP")
+    <*> switch (long "discord" <> short 'd' <> help "Discord bot")
